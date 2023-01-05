501ca902a4eac71f60abf10334e0d0e7b643b5d3
lglebk <102877030+lglebk@users.noreply.github.com>
2023-01-05 14:33:01 +0300

    built: finished mobile header with JS

---
+++ source/js/app.js
@@ -0,0 +1,14 @@
+const navElement = document.querySelector('.header__nav');
+const buttonElement = document.querySelector('.nav__toggle');
+
+navElement.classList.add('header__nav--closed');
+
+buttonElement.addEventListener('click', () => {
+  if (navElement.classList.contains('header__nav--closed')) {
+    navElement.classList.add('header__nav--opened');
+    navElement.classList.remove('header__nav--closed');
+  } else {
+    navElement.classList.add('header__nav--closed');
+    navElement.classList.remove('header__nav--opened');
+  }
+});
